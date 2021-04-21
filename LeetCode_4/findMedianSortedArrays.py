48b6b160408fa9ef57e4f819cfbe26135f8c6d96
H <416362177@qq.com>
2021-04-21 14:54:40 +0800

    add LeetCode_4
    add LeetCode_4

---
+++ LeetCode_4/findMedianSortedArrays.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/20 17:54
+# @Author  : H
+# @File    : findMedianSortedArrays.py
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+
+if __name__ == '__main__':
+    nums1 = [1, 2]
+    nums2 = [3, 4]
+    solution = Solution()
+    print(solution.findMedianSortedArrays(nums1,nums2))
