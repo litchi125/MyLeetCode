d0765dda7b9f8eee10c746c41c7df149fd728790
H <416362177@qq.com>
2021-04-21 16:31:44 +0800

    LeetCode_4:First Commit

--- LeetCode_4/findMedianSortedArrays.py
+++ LeetCode_4/findMedianSortedArrays.py
@@ -8,11 +8,27 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        pass
+        counts, flag = divmod(len(nums1) + len(nums2) + 1, 2)
+        print(counts,flag)
+        counts = counts + flag
+        print(counts)
+        result = 0
+        while counts:
+            counts -= 1
+            if nums1 and nums2:
+                min_p = nums1.pop() if nums1[-1] > nums2[-1] else nums2.pop()
+            elif nums1 and not nums2:
+                min_p = nums1.pop()
+            elif nums2 and not nums1:
+                min_p = nums2.pop()
+            if flag == 1 and counts < 2:
+                result = (result + min_p)
+        result = result / 2 if flag == 1 else min_p
+        return result
 
 
 if __name__ == '__main__':
     nums1 = [1, 2]
-    nums2 = [3, 4]
+    nums2 = [3]
     solution = Solution()
-    print(solution.findMedianSortedArrays(nums1,nums2))
+    print(solution.findMedianSortedArrays(nums1, nums2))
